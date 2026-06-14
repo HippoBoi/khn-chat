a7cafd1ab7cf090bddc997288daf30c440887c0b
Andy <tagrifavc@gmail.com>
2026-06-13 23:32:04 -0400

    hide chat until first connection

--- react-chat/src/App.tsx
+++ react-chat/src/App.tsx
@@ -4,12 +4,22 @@ import { MessageList } from './components/MessageList';
 import { MessageInput } from './components/MessageInput';
 import { UsernameForm } from './components/UsernameForm';
 import { ProfilePicturePicker } from './components/ProfilePicturePicker';
+import { useChatStore } from './store/useChatStore';
 
 import './App.css';
 import { Title } from './components/Title';
 
 function App() {
   useSocket();
+  const isChatVisible = useChatStore((s) => s.isChatVisible);
+
+  if (!isChatVisible) {
+    return (
+      <div className="app-container">
+        <p>Connecting to server...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="app-container">
